57a4b35444ba9a111022dd12530d9aa5a940da8f
SandActor <88283373+SandActor@users.noreply.github.com>
2024-09-23 07:16:10 +0000

    Первая игра и запись в asciinema

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SandActor/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/SandActor/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/127dea9de937e3ac59a9/maintainability)](https://codeclimate.com/github/SandActor/frontend-project-44/maintainability)
+https://asciinema.org/a/YnDZYA8pg4gKg2c48x3sH96Ml
---
+++ bin/brain-even.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+console.log(`Answer "yes" if the number is even, otherwise answer "no".`)
+let counterToCorrect = 0;
+while (counterToCorrect < 3) {
+    let number = Math.floor(Math.random() * 30);
+    console.log(`Question: ${number}`);
+    let correctAnswer = "yes";
+    const answer = readlineSync.question('Your answer: ');
+
+    if (number%2 !== 0){
+        correctAnswer = "no";
+    }
+    if (answer === correctAnswer){
+        console.log(`Correct!`);
+        counterToCorrect++;
+    }
+    else {
+        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.`);
+        console.log(`Let's try again, ${name}!`);
+        break;
+    }
+}
+if(counterToCorrect === 3) {
+    console.log(`Congratulations, ${name}!`)
+}
