8053fbe95a09b9a3c00e23483cce073f2d6d3d1b
SandActor <88283373+SandActor@users.noreply.github.com>
2024-11-08 07:49:56 +0000

    а так?

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-export const getRandomNumber = (maxNumber) =>
- Math.floor(Math.random() * maxNumber);
+export const getRandomNumber = (maxNumber) => Math.floor(Math.random() * maxNumber);
 
-export const checkParity = (number) =>
- number % 2 !== 0;
+export const checkParity = (number) => number % 2 !== 0;
